6622b5297d2e81b3ef4248b64211fa8fd6c14d28
William Benarto <williambenarto@Williams-MacBook-Pro.local>
2025-07-08 10:34:33 -0700

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900">
       <div className="text-center">
-        <img 
+        <Image 
           src="/images/logo-main.png" 
           alt="Fortia Logo" 
-          className="mx-auto w-200 h-200"
+          width={200} 
+          height={200}
+          className="mx-auto"
         />
       </div>
     </div>
