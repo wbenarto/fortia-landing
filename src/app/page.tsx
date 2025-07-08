cfcfed03ae4c1daa4510402f778d5bdf43040cb2
William Benarto <williambenarto@Williams-MacBook-Pro.local>
2025-07-08 10:38:56 -0700

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900">
+    <div className="flex justify-center min-h-screen pt-[150px] bg-white dark:bg-gray-900">
       <div className="text-center">
         <Image 
           src="/images/logo-main.png" 
           alt="Fortia Logo" 
-          width={200} 
-          height={200}
-          className="mx-auto"
+          width={600} 
+          height={100}
+          className="mx-auto "
         />
+        <h1 className="text-5xl font-bold text-gray-900 dark:text-white">Coming soon!</h1>
       </div>
     </div>
   );
