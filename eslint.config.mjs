4f4bdf4fc6acab74c7aa696108a6f3f4361ceb9a
wbenarto <w.benarto@gmail.com>
2025-09-02 22:32:13 -0700

    support page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,7 +40,7 @@ const eslintConfig = [
 
       // React specific rules
       'react-hooks/exhaustive-deps': 'warn',
-      'react/no-unescaped-entities': 'error',
+      'react/no-unescaped-entities': 'warn',
       'react/jsx-key': 'error',
       'react/jsx-no-duplicate-props': 'error',
       'react/jsx-no-undef': 'error',
